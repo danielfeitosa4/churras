dff1d2bac928dbea540e4fc544e487de9c64f8d6
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2022-12-30 23:11:55 -0300

    calculo de 'total de carne' e 'carne por pessoa'

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,30 @@
 // Refrigerante/agua - 1000 ml por pessoa + 6 horas 1500ml
 
 // Crianças valem por 0,5
+
+let inputAdults = document.getElementById("adults");
+let inputKids = document.getElementById("kids");
+let inputDuration = document.getElementById("duration");
+
+let results = document.getElementById("results");
+
+let calculate = () => {
+    console.log("calculando...");
+
+    let adults = inputAdults.value;
+    let kids = inputKids.value;
+    let duration = inputDuration.value;
+
+    let totalAmountOfMeat = meatPerPerson(duration) * adults + (meatPerPerson(duration)/2 * kids);
+    console.log(totalAmountOfMeat);
+}
+
+let meatPerPerson = (duration) => {
+
+    if(duration >= 6){
+        return 650;
+    }else {
+        return 400;
+    }
+
+}
